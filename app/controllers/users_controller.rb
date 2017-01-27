0e584e5f53e934f26768779c77b6fa2da6f82b60
Mark Latham <mark@votermedia.org>
2017-01-27 15:35:37 -0800

    Followed devise setup instructions for controllers.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+  
+  before_action :authenticate_user!
+  after_action :verify_authorized
+
+  def index
+    @users = User.all
+    authorize User
+  end
+
+  def show
+    @user = User.find(params[:id])
+    authorize @user
+  end
+
+end
