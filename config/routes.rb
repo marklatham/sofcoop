bc69a973fe779b76dc59839737f6df3543c3acd1
Mark Latham <mark@votermedia.org>
2017-02-01 17:55:41 -0800

    github.com/plataformatec/devise#configuring-controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  devise_for :users, path: '', path_names: {sign_up: 'register', sign_in: 'login', sign_out: 'logout'}
+  devise_for :users, controllers: { sessions: 'users/sessions' }, path: '', path_names: {sign_up: 'register', sign_in: 'login', sign_out: 'logout'}
   
   root 'visitors#index'
   
