2a058105159226451f27109d51d9d0dda22e27bd
Mark Latham <mark@votermedia.org>
2017-02-27 15:18:51 -0800

    Refactor: Copied entire Devise::RegistrationsController because making so many customizations. Also fixed bug in create re initial username.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,25 +1,47 @@
-class Users::RegistrationsController < Devise::RegistrationsController
+class Users::RegistrationsController < DeviseController
 # before_action :configure_sign_up_params, only: [:create]
   before_action :configure_account_update_params, only: [:update]
+  prepend_before_action :require_no_authentication, only: [:new, :create, :cancel]
+  prepend_before_action :authenticate_scope!, only: [:edit, :update, :destroy]
+  prepend_before_action :set_minimum_password_length, only: [:new, :edit]
 
   # GET /resource/sign_up
   def new
     @body_class = 'grayback'
-    super
+    build_resource({})
+    respond_with resource
   end
 
   # POST /resource
   def create
-    super do |resource|
-      resource.username = 'user' + resource.id.to_s
-      resource.save
+    build_resource(sign_up_params)
+    # Username can't be nil, but want to use id, so guess it:
+    resource.username = 'user' + User.maximum(:id).next.to_s
+    resource.save
+    # Then correct the guess in case previous user deleted:
+    resource.username = 'user' + resource.id.to_s
+    resource.save
+    if resource.persisted?
+      if resource.active_for_authentication?
+        set_flash_message! :notice, :signed_up
+        sign_up(resource_name, resource)
+        respond_with resource, location: after_sign_up_path_for(resource)
+      else
+        set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+        expire_data_after_sign_in!
+        respond_with resource, location: after_inactive_sign_up_path_for(resource)
+      end
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      respond_with resource
     end
   end
 
   # GET /resource/edit
   def edit
     @body_class = 'grayback'
-    super
+    render :edit
   end
 
   def change_password
@@ -27,7 +49,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   # PUT /resource
-  def update # TO DO: disallow username = "user*" unless = "user" + id.to_s
+  # We need to use a copy of the resource because we don't want to change
+  # the current user in place.
+  def update
     self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
     prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
 
@@ -63,16 +87,19 @@ class Users::RegistrationsController < Devise::RegistrationsController
     end
   end
 
-  # DELETE /resource
-  
+  # GET /cancel_account
   def cancel_account
     self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
     @pages = Page.where(user_id: resource.id)
     @body_class = 'grayback'
   end
 
+  # DELETE /resource
   def destroy
-    super
+    resource.destroy
+    Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name)
+    set_flash_message! :notice, :destroyed
+    respond_with_navigational(resource){ redirect_to after_sign_out_path_for(resource_name) }
     AdminMailer.account_cancelled(resource).deliver  # notify admin
   end
 
@@ -82,34 +109,83 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # cancel oauth signing in/up in the middle of the process,
   # removing all OAuth session data.
   def cancel
-    super
+    expire_data_after_sign_in!
+    redirect_to new_registration_path(resource_name)
   end
   
-  # protected
+  protected
+
+  def update_needs_confirmation?(resource, previous)
+    resource.respond_to?(:pending_reconfirmation?) &&
+      resource.pending_reconfirmation? &&
+      previous != resource.unconfirmed_email
+  end
+
+  # By default we want to require a password checks on update.
+  # You can overwrite this method in your own RegistrationsController.
+  def update_resource(resource, params)
+    resource.update_with_password(params)
+  end
+
+  # Build a devise resource passing in the session. Useful to move
+  # temporary session data to the newly created user.
+  def build_resource(hash=nil)
+    self.resource = resource_class.new_with_session(hash || {}, session)
+  end
+
+  # Signs in a user on sign up. You can overwrite this method in your own
+  # RegistrationsController.
+  def sign_up(resource_name, resource)
+    sign_in(resource_name, resource)
+  end
+
+  # The path used after sign up. You need to overwrite this method
+  # in your own RegistrationsController.
+  def after_sign_up_path_for(resource)
+    after_sign_in_path_for(resource)
+  end
+
+  # The path used after sign up for inactive accounts. You need to overwrite
+  # this method in your own RegistrationsController.
+  def after_inactive_sign_up_path_for(resource)
+    scope = Devise::Mapping.find_scope!(resource)
+    router_name = Devise.mappings[scope].router_name
+    context = router_name ? send(router_name) : self
+    context.respond_to?(:root_path) ? context.root_path : "/"
+  end
+
+  # The default url to be used after updating a resource. You need to overwrite
+  # this method in your own RegistrationsController.
+  def after_update_path_for(resource)
+    signed_in_root_path(resource)
+  end
+
+  # Authenticates the current scope and gets the current resource from the session.
+  def authenticate_scope!
+    send(:"authenticate_#{resource_name}!", force: true)
+    self.resource = send(:"current_#{resource_name}")
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
   #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
   # end
 
+  def sign_up_params
+    devise_parameter_sanitizer.sanitize(:sign_up)
+  end
+
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
   end
 
-  # The path used after sign up.
-  def after_sign_up_path_for(resource)
-    super(resource)
+  def account_update_params
+    devise_parameter_sanitizer.sanitize(:account_update)
   end
 
-  # The path used after sign up for inactive accounts.
-  def after_inactive_sign_up_path_for(resource)
-    super(resource)
-  end
-
-  # The default url to be used after updating a resource.
-  def after_update_path_for(resource)
-    super(resource)
+  def translation_scope
+    'devise.registrations'
   end
   
 end
