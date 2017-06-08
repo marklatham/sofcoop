2bf238e346feae6134246508aa34bf33778758a3
Mark Latham <mark@votermedia.org>
2017-06-08 11:19:41 -0700

    Gem omniauth-twitter: install but not set up yet.

--- config/initializers/friendly_id.rb
+++ config/initializers/friendly_id.rb
@@ -17,7 +17,7 @@ FriendlyId.defaults do |config|
   config.use :reserved
 
   config.reserved_words = %w(new edit index session login logout users admin
-    stylesheets assets javascripts images profile timeline posts)
+    stylesheets assets javascripts images profile timeline posts sofcoop)
 
   #  ## Friendly Finders
   #
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170531194940) do
+ActiveRecord::Schema.define(version: 20170607171121) do
 
   create_table "channels", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id"
@@ -138,6 +138,8 @@ ActiveRecord::Schema.define(version: 20170531194940) do
     t.string   "username",                            null: false
     t.string   "slug",                                null: false
     t.string   "avatar"
+    t.string   "provider"
+    t.string   "uid"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
