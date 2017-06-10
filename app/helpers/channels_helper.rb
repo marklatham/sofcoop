8637d93c0b55348d13663a47dc26a58dd162386d
Mark Latham <mark@votermedia.org>
2017-06-10 15:47:08 -0700

    Show default avatar for channel with no uploaded avatar.

--- app/helpers/channels_helper.rb
+++ app/helpers/channels_helper.rb
@@ -5,7 +5,7 @@ module ChannelsHelper
       substrings = channel.avatar_url.partition('?').first.partition('/channels/')
       avatar_url = substrings[1] + '@@' + substrings[2]
     else
-      avatar_url = ""
+      avatar_url = "https://sofcoop.s3-us-west-2.amazonaws.com/channels/default.png"
     end
     image_tag(avatar_url.to_s, alt: '@@'+channel.name)
   end
