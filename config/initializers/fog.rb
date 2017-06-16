024b6fbfb376e8b53ddd39542ce6aebbce04ffc1
Mark Latham <mark@votermedia.org>
2017-06-16 11:22:04 -0700

    AWS-fog credentials: Tidied up: Stop using application.yml (gitignored btw). Reference secrets in initializers/fog.rb.

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -4,10 +4,10 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     provider:              'AWS',
-    aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    aws_access_key_id:     Sofcoop::Application.secrets.aws_access_key_id,
+    aws_secret_access_key: Sofcoop::Application.secrets.aws_secret_access_key,
     region:                'us-west-2'
   }
-  config.fog_directory   = ENV['AWS_S3_BUCKET']
+  config.fog_directory   = Sofcoop::Application.secrets.aws_s3_bucket
   config.fog_public      = false
 end
