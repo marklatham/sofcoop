54224d8aac84007a550900755280b2661cbb788f
Mark Latham <mark@votermedia.org>
2017-06-28 10:21:07 -0700

    Unified OminAuth provider button styles.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -247,12 +247,12 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :twitter, Sofcoop::Application.secrets.twitter_api_key,
-                  Sofcoop::Application.secrets.twitter_api_secret
-  config.omniauth :facebook, Sofcoop::Application.secrets.facebook_api_key,
-                  Sofcoop::Application.secrets.facebook_api_secret
+  config.omniauth :twitter,       Sofcoop::Application.secrets.twitter_api_key,
+                                  Sofcoop::Application.secrets.twitter_api_secret
+  config.omniauth :facebook,      Sofcoop::Application.secrets.facebook_api_key,
+                                  Sofcoop::Application.secrets.facebook_api_secret
   config.omniauth :google_oauth2, Sofcoop::Application.secrets.google_api_key,
-                  Sofcoop::Application.secrets.google_api_secret
+                                  Sofcoop::Application.secrets.google_api_secret
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
